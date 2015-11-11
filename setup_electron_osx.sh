884ba915eaa7eb5c6140d2559637ead4227c52aa
Matthew Kanwisher <matt@kanwisher.com>
2015-11-11 13:50:58 +0700

    closer to code signing

--- setup_electron_osx.sh
+++ setup_electron_osx.sh
@@ -9,6 +9,13 @@ mv staging/Electron.app staging/MonkeyType.app
 
 png2icsns.sh platforms/osx/icon-3.png
 mv icon-3.icns staging/MonkeyType.app/Contents/Resources/atom.icns
+rm -rf "staging/MonkeyType.app/Contents/Frameworks/Squirrel.framework/Resources/ShipIt"
+rm -rf "staging/MonkeyType.app/Contents/Frameworks/Electron Framework.framework/Resources/crashpad_handler"
+rm "./staging/MonkeyType.app/Contents/Frameworks/Electron Framework.framework/Versions/A/Resources/crashpad_handler"
+rm "./staging/MonkeyType.app/Contents/Frameworks/Electron Framework.framework/Versions/Current/Resources/crashpad_handler"
+rm "./staging/MonkeyType.app/Contents/Frameworks/Squirrel.framework/Versions/A/Resources/ShipIt"
+rm "./staging/MonkeyType.app/Contents/Frameworks/Squirrel.framework/Versions/Current/Resources/ShipIt"
+
 #TODO setup helpers
 #Electron Helper EH.app
 
